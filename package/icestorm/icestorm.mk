58d6816dc7a1dbec652a3ef1b18c2e7b89d11d27
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2021-11-26 06:48:55 +0000

    package/icestorm: new PoC package

---
+++ package/icestorm/icestorm.mk
@@ -0,0 +1,53 @@
+################################################################################
+#
+# icestorm
+#
+################################################################################
+ICESTORM_VERSION = 83b8ef947f77723f602b706eac16281e37de278c
+ICESTORM_SITE = $(call github,YosysHQ,icestorm,$(ICESTORM_VERSION))
+
+ICESTORM_LICENSE = MIT
+ICESTORM_LICENSE_FILES = COPYING
+
+ICESTORM_DEPENDENCIES = python3
+
+# For third-party, icestorm is mandatory at
+# compile time.
+ICESTORM_INSTALL_STAGING = YES
+
+
+ICESTORM_MAKE_OPTS=PREFIX=/usr \
+	PYTHON3=$(HOST_DIR)/bin/python3 \
+	DEBUG=0 \
+	ICEPROG=0 \
+	PKG_CONFIG=$(PKG_CONFIG_HOST_BINARY) \
+	STATIC=0 \
+	CHIPDB_SUBDIR=icebox
+
+ifeq ($(BR2_PACKAGE_ICESTORM_ICEPROG),y)
+ICESTORM_MAKE_OPTS += ICEPROG=1
+ICESTORM_DEPENDENCIES += host-pkgconf libftdi1
+else
+ICESTORM_MAKE_OPTS += ICEPROG=0
+endif
+
+define ICESTORM_CONFIGURE_CMDS
+	echo -n "" > $(@D)/config.mk
+endef
+
+define ICESTORM_BUILD_CMDS
+   	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		$(ICESTORM_MAKE_OPTS)
+endef
+
+define ICESTORM_INSTALL_TARGET_CMDS
+   	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		$(ICESTORM_MAKE_OPTS) DESTDIR=$(TARGET_DIR) install
+endef
+
+define ICESTORM_INSTALL_STAGING_CMDS
+   	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		$(ICESTORM_MAKE_OPTS) DESTDIR=$(STAGING_DIR) install
+endef
+
+$(eval $(generic-package))
