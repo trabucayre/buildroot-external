18f0eced6792f878902874abeb694837cd768076
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2023-02-17 06:27:36 +0000

    package/icestorm: fix build with sourcery-mipsXX toolchains, check-package compliant

--- package/icestorm/icestorm.mk
+++ package/icestorm/icestorm.mk
@@ -3,6 +3,7 @@
 # icestorm
 #
 ################################################################################
+
 ICESTORM_VERSION = 8649e3e0bd0e09429898d2569ef65cc9fd3eafd7
 ICESTORM_SITE = $(call github,YosysHQ,icestorm,$(ICESTORM_VERSION))
 
@@ -15,39 +16,40 @@ ICESTORM_DEPENDENCIES = python3
 # compile time.
 ICESTORM_INSTALL_STAGING = YES
 
-
-ICESTORM_MAKE_OPTS=PREFIX=/usr \
-	PYTHON3=$(HOST_DIR)/bin/python3 \
-	DEBUG=0 \
-	ICEPROG=0 \
-	PKG_CONFIG=$(PKG_CONFIG_HOST_BINARY) \
-	STATIC=0 \
-	CHIPDB_SUBDIR=icebox
-
 ifeq ($(BR2_PACKAGE_ICESTORM_ICEPROG),y)
-ICESTORM_MAKE_OPTS += ICEPROG=1
+ICESTORM_ENABLE_ICEPROG = 1
 ICESTORM_DEPENDENCIES += host-pkgconf libftdi1
 else
-ICESTORM_MAKE_OPTS += ICEPROG=0
+ICESTORM_ENABLE_ICEPROG = 0
 endif
 
 define ICESTORM_CONFIGURE_CMDS
 	echo -n "" > $(@D)/config.mk
+	echo "PREFIX ?= /usr" >> $(@D)/config.mk
+	echo "DEBUG ?= 0" >> $(@D)/config.mk
+	echo "ICEPROG ?= $(ICESTORM_ENABLE_ICEPROG)" >> $(@D)/config.mk
+	echo "CXX ?= $(TARGET_CXX)" >> $(@D)/config.mk
+	echo "CC ?= $(TARGET_CC)" >> $(@D)/config.mk
+	echo "PKG_CONFIG=$(PKG_CONFIG_HOST_BINARY)" >> $(@D)/config.mk
+	echo "LDLIBS = -lm -lstdc++" >> $(@D)/config.mk
+	echo "CFLAGS += $(TARGET_CFLAGS) -MD -MP -std=c99" >> $(@D)/config.mk
+	echo "CXXFLAGS += $(TARGET_CXXFLAGS) -MD -MP -std=c++11" >> $(@D)/config.mk
+	echo "CHIPDB_SUBDIR ?= icebox" >> $(@D)/config.mk
+	echo "PYTHON3 ?= $(HOST_DIR)/bin/python3" >> $(@D)/config.mk
 endef
 
 define ICESTORM_BUILD_CMDS
-   	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-		$(ICESTORM_MAKE_OPTS)
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
 endef
 
 define ICESTORM_INSTALL_TARGET_CMDS
-   	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-		$(ICESTORM_MAKE_OPTS) DESTDIR=$(TARGET_DIR) install
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		DESTDIR=$(TARGET_DIR) install
 endef
 
 define ICESTORM_INSTALL_STAGING_CMDS
-   	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-		$(ICESTORM_MAKE_OPTS) DESTDIR=$(STAGING_DIR) install
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		DESTDIR=$(STAGING_DIR) install
 endef
 
 $(eval $(generic-package))
